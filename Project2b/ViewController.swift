3b69b2b372845a8109d03f7bce4543a37a31dc62
VictorQi <qijianqiong@gmail.com>
2017-10-09 19:15:53 +0800

    LocationManager
    fetch location and heading

--- Project2b/ViewController.swift
+++ Project2b/ViewController.swift
@@ -9,13 +9,26 @@
 import UIKit
 import SpriteKit
 import ARKit
+import CoreLocation
+import GameplayKit
+import SwiftyJSON
 
-class ViewController: UIViewController, ARSKViewDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet var sceneView: ARSKView!
     
+    let locationManager = CLLocationManager()
+    var userLocation = CLLocation()
+    var sightsJSON: JSON!
+    var userHeading = 0.0
+    var headingCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -34,7 +47,7 @@ class ViewController: UIViewController, ARSKViewDelegate {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = AROrientationTrackingConfiguration()
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -52,14 +65,30 @@ class ViewController: UIViewController, ARSKViewDelegate {
         // Release any cached data, images, etc that aren't in use.
     }
     
-    // MARK: - ARSKViewDelegate
+    func fetchSights() {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(userLocation.coordinate.latitude)%7C\(userLocation.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        if let data = try? Data(contentsOf: url) {
+            sightsJSON = JSON(data)
+            locationManager.startUpdatingHeading()
+        }
+    }
     
+    func createSights() {
+        
+    }
+    
+}
+
+// MARK: - ARSKViewDelegate
+extension ViewController: ARSKViewDelegate {
     func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
         // Create and configure a node for the anchor added to the view's session.
-        let labelNode = SKLabelNode(text: "👾")
-        labelNode.horizontalAlignmentMode = .center
-        labelNode.verticalAlignmentMode = .center
-        return labelNode;
+        
+        return nil
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
@@ -77,3 +106,43 @@ class ViewController: UIViewController, ARSKViewDelegate {
         
     }
 }
+
+// MARK: - CLLocationManagerDelegate
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager.requestLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {
+            return
+        }
+        
+        userLocation = location
+        
+        DispatchQueue.global().async {
+            self.fetchSights()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        DispatchQueue.main.async {
+            self.headingCount += 1
+            if self.headingCount != 2 {
+                return
+            }
+            
+            self.userHeading = newHeading.magneticHeading
+            self.locationManager.stopUpdatingHeading()
+            self.createSights()
+        }
+    }
+}
+
+
